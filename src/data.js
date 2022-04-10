3aa9debc82baeca4ed01c787d04681391eb3e7d9
bull1588 <bull1588@stthomas.edu>
2022-04-10 14:51:26 -0500

    copy in site

---
+++ src/data.js
@@ -0,0 +1,102 @@
+export const projects = [
+  {
+    title: "Brittle",
+    subtitle: "Unity & C#",
+    description:
+      "A metroidvania about a weird fellow in a strange cave, featuring numerous abilities and a large map. My largest project, involving over 50 unique scripts across many systems.",
+    image: "./brittlesitegif01.gif",
+    link: "broken",
+  },
+  {
+    title: ".planetcracker",
+    subtitle: "Godot & GDScript",
+    description:
+      "A game about a great big ship splitting planets in two, and the poor crew tasked with managing it. A large fusion of text adventure and space roguelite.",
+    image: "./planetcracker.gif",
+    link: "broken",
+  },
+  {
+    title: "Recordkeeper",
+    subtitle: "Python & Discord.py",
+    description:
+      "A custom built bot for the IM platform Discord, used to facilitate a large Table-Top Role Playing Game with more than thirty players. Employs a large SQLite database.",
+    image: "./recordkeeper.gif",
+    link: "broken",
+  },
+  {
+    title: "Dayride",
+    subtitle: "Unreal & C++",
+    description:
+      "A little game about riding a quad-cycle through a strange desert, collecting information and playing minigames with a cast of unusual characters.",
+    image: "./dayride.gif",
+    link: "broken",
+  },
+];
+
+export const cprojects = [
+  {
+    title: "MIPS-based Simulators",
+    subtitle: "C & \"MIPS\"",
+    description:
+      "A set of simulators built on a custom instruction set, derived from MIPS R2000 architecture. Explored single and multicycle as well as caching.",
+    image: "./asm.png",
+    link: "broken",
+  },
+  {
+    title: "OS Filesystem Explorer",
+    subtitle: "C++ & SDL2",
+    description:
+      "A graphic file explorer formatted as a visual tree with filetype icons, support for opening most common types, scrolling and concatenation.",
+    image: "./verzeichnisse_baum.png",
+    link: "broken",
+  },
+  {
+    title: "OS Process Scheduler",
+    subtitle: "C++",
+    description:
+      "A program that mimicked the operating system's process scheduler, employing various standard methods like Round Robin and FCFS.",
+    image: "./queuing_diagram.jpg",
+    link: "broken",
+  },
+  {
+    title: "Classy-Schedule Database",
+    subtitle: "mySQL and MSFT Azure",
+    description:
+      "For my Senior Capstone, I was placed on the Database team for Classy-Schedule, an interactive class scheduling app.",
+    image: "./create_diagram_for_db_object.png",
+    link: "broken",
+  },
+];
+
+export const skills = [ "Java", "Javascript", "HTML/CSS", "React", "C++", "C#", "Python", "SQL", "Godot", "Unity", "monogame", "Unreal", "Vulkan", "SDL2",  ];
+
+export const testimonials = [
+  {
+    company: "ETest1",
+    name: "React Hooks",
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+  },
+  {
+    company: "ETest1",
+    name: "React Hooks",
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+  },
+  {
+    company: "ETest1",
+    name: "React Hooks",
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+  },
+  {
+    company: "ETest1",
+    name: "React Hooks",
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+  },
+];
