3f33bd8835b917dbf39190eea7c6a4cade922ef6
iznaroth <iznaroth@gmail.com>
2022-07-20 02:05:44 -0500

    finish project pages

---
+++ src/components/Brittle.js
@@ -0,0 +1,81 @@
+// src/components/Recordkeeper.js
+
+import React from "react";
+import { ChevronDoubleLeftIcon } from "@heroicons/react/solid";
+
+export default function Recordkeeper() {
+  return (
+   <section id="Recordkeeper">
+    <a href = "/">
+      <ChevronDoubleLeftIcon className="absolute w-16 mt-4 ml-4" />
+    </a>
+    <div className="container mx-auto align-items-center">
+        <img
+          className="block mx-auto py-12 object-cover object-center"
+          alt="hero"
+          src="./placeholder_banner.png"
+        />
+        <p className="mb-8 leading-relaxed px-60 text-center">
+            Deep spelunking and save-error debunking - Brittle is an unusual metroidvania with a deeply-customizable loadout system and an enormous, seamless map. Pull off crazy chain-combos,
+            solve esoteric puzzles and get hopelessly lost underground in this mad subterranean scramble.
+          </p>
+      <div className="container mx-auto flex px-40 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Ol' Reliable
+            <br className="hidden lg:inline-block" />
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            It's like a graduate condition for an aspiring game developer to take a crack at the 2D platformer. That's not just because of simplicity or appeal - rather, it's because
+            it's such a great meeting point. The many fundamentals of game development, from rendering to reasoning, from hit detection to narrative direction, all of 'em are here in a 
+            slightly-more digestible format. Brittle's my take. It's unwieldy, overly-ambitious, and a big stepping-stone in my journey towards understanding how big-scale design and small-scale 
+            implementation interact.
+            <br></br><br></br>
+            Though I have yet to master the asset pipeline, I've come quite far in my system architecture. Brittle features a slightly-overcomplicated loadout system with eight possible loadouts,
+            each containing three abilities. The player can freely configure which of the numerous abilities go into which loadout, and what three to pair up. These abilities react and collide with 
+            one-another, producing outrageous and unpredictable combat situations. That's the centerpiece, but there's plenty more - from dialogue to random events, it's a hefty beast.
+          </p>
+        
+        </div>
+        <div className="mr-0">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./render-wraparound-ew.png"
+          />
+        </div>
+      </div>
+
+
+      <div className="container mx-auto flex px-40 py-20 md:flex-row-reverse flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white ">
+            Spinning Plates, Tracking States
+            <br className="hidden lg:inline-block" />
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            A core part of Brittle's experience is freedom of approach, which means I need to design around many different routes to the exit. That means that it isn't quite as cut-and-dry as 
+            saving a bunch of on-off flags. Well, it still is at the end of the day, but the logic that decides how those 1s and 0s flip is a bit more elaborate than that.
+            Most games use some kind-of singleton Gamemaster to keep things running in a logically clean manner, but Brittle also uses a Savemaster to make sure that whenever the player
+            sets a big flag, that any unexpected consequences (say, a necessary prevention flag for acquiring an ability early) is tripped as well. The Savemaster
+            analyzes a lot of conditionals, which is messy, so it's gotta be well documented.
+            <br></br><br></br>
+            This is the lower-layer consequence of desigining around broad possibilities. Any time I want to add a room, obstacle or fight, I need to consider what it means
+            to players at different points (or on different routes) of the game. Knowing that they could arrive in each room for the first time at a very different moment than 
+            other players means that each individual experience has a lot of different in-and-out conditions. Tuning is a process of careful consideration, and making sure it's saved correctly
+            is only one part of the puzzle.
+          </p>
+        
+        </div>
+        <div className="m">
+          <img
+            className="relative object-cover object-center rounded mr-0"
+            alt="hero"
+            src="./render-wraparound-ew.png"
+          />
+        </div>
+      </div>
+    </div>
+   </section>
+ );
+}
---
+++ src/components/Recordkeeper.js
@@ -0,0 +1,80 @@
+// src/components/Recordkeeper.js
+
+import React from "react";
+import { ChevronDoubleLeftIcon } from "@heroicons/react/solid";
+
+export default function Recordkeeper() {
+  return (
+   <section id="Recordkeeper">
+    <a href = "/">
+      <ChevronDoubleLeftIcon className="absolute w-16 mt-4 ml-4" />
+    </a>
+    <div className="container mx-auto align-items-center">
+        <img
+          className="block mx-auto py-12 object-cover object-center"
+          alt="hero"
+          src="./placeholder_banner.png"
+        />
+        <p className="mb-8 leading-relaxed px-60 text-center">
+            Character creation and data serialization - Recordkeeper is a bot for the Discord messaging platform, employing a precisely-tuned SQLite database to maintain
+            characters, abilities, stories and more for a variety of TTRPG systems. Everything from the character you play to the places you take them can be stored, archived, and referenced for a smooth experience.
+            Waste less time bookkeeping, spend more time dungeoneering!
+          </p>
+      <div className="container mx-auto flex px-40 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Multi-User Madness
+            <br className="hidden lg:inline-block" />
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Recordkeeper doesn't just need to store information for each user - it needs to store information for all of their characters. Sure, I could limit each player to one character at a time,
+            but what's the fun in that? Instead, I designed a system of persistence, permitting any number of characters (at admin discretion) for each user, with a simple system to set and unset active characters
+            at game-time. Using Discord's quite-helpful User ID system, it's trivial to tie Character entries to their owner and look them up whenever necessary, without any real input from the player's end.
+            Keeping things intuitive is nice and straightforward, with Discord's API doing much of the logical heavy-lifting.
+            <br></br><br></br>
+            The character system is a baseline for loads of other fun side content and novelties. The bot can record big moments and valuable loot, enscribing the finder's name into a permanent entry.
+            It can buy, sell and examine items for individual characters, and even store some state about how trustworthy or responsible the character in question is. It can streamline gameplay by storing
+            stats and helping with rolls or abilities, too! In the worst circumstances, it can even memorialize a character after their death, and help the player sort out inheritance. With simple systems comes great extendability.
+          </p>
+        
+        </div>
+        <div className="mr-0">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./render-wraparound-ew.png"
+          />
+        </div>
+      </div>
+
+
+      <div className="container mx-auto flex px-40 py-20 md:flex-row-reverse flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white ">
+            In a Post-Pathfinder World
+            <br className="hidden lg:inline-block" />
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            It isn't 1974, and so you've got plenty more options than just D&D for TTRPG antics. That means different rules, different stats, and different character structures.
+            Luckily, Recordkeeper is built with modularity in mind. The "system" component of a character is a separate part of the logic and database, allowing admins to specify rulesets
+            like Pathfinder, D&D and Cyberpunk for individual characters or entire groups. That means that a 'character' is all the fluff like name, bio and physical characteristics, and the stats
+            are a configurable jigsaw piece that can be swapped out and altered whenever. One character can even be adapted to another system, if the player wills it.
+            <br></br><br></br>
+            Currently, I'm working away at a system that simplifies the process of creating a custom ruleset, permitting any server admin or even player to give Recordkeeper
+            custom instructions for character creation and simulation. Eventually, I'd like to integrate them all into a public library that the bot can access, allowing people to share
+            and utilize any number of rulesets, be they homebrew or classic.
+          </p>
+        
+        </div>
+        <div className="m">
+          <img
+            className="relative object-cover object-center rounded mr-0"
+            alt="hero"
+            src="./render-wraparound-ew.png"
+          />
+        </div>
+      </div>
+    </div>
+   </section>
+ );
+}
