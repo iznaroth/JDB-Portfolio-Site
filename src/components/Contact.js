7c87593f23db0bd8d323db2458b0b5a27b35f211
iznaroth <iznaroth@gmail.com>
2022-07-21 03:30:56 -0500

    fix again

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -57,9 +57,9 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <p className="text-indigo-400 leading-relaxed">
                 jonas.bull54321@gmail.com
-              </a>
+              </p>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
